29c90f2917b59f05e5670c32042e8fd45d194372
Karan Verma <karan.verma@catalystone.com>
2023-05-24 10:54:25 +0530

    Added header cart functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
     return <>
         <header className={classes.header}>
             <h1>Meals</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart} />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt='Meal' />
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,21 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon.js';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context.js';
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
+    const cartContext = useContext(CartContext);
+    const noOfCartItems = cartContext.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return <>
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span></span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{noOfCartItems}</span>
         </button>
     </>
 }
